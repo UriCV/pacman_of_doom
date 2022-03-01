f3394b807086f1e9397d11aea112558ff11026b1
MADROCHA <madlarocha@gmail.com>
2022-03-01 16:10:59 +0100

    fix errors html and css, and start fix array for random names

--- js/pacman.js
+++ js/pacman.js
@@ -1,13 +1,13 @@
-const addButton = document.getElementById("add-btn");
-const deleteButton = document.getElementById('delete-btn');
-const inputName;
 
 let arrayNames = ["Ingrid", "Uri", "Guillermo", "Ales", "Albert", "Adrian", "Dani", "Yeray", "Jess", "Alex", "Scarlet","Sergi"];
 let result;
 function randomArrayNames(){
+    console.log(arrayNames);
     result = arrayNames[Math.floor(Math.random()*arrayNames.length)];
-    arrayNames.splice(result);
-    console.log(result);
+    console.log("Abans d'splice: " + result);
+    arrayNames.splice(result, 1);
+    console.log("Després d'splice: " + result);
+    console.log(arrayNames);
     document.getElementById("names").innerHTML=result;
     document.getElementById("list").innerHTML+=`<li>${result}</li>`;
 }
