585138e5120604161076b340515d180decb2b83c
Ales <aleswebgit@gmail.com>
2022-03-02 14:48:00 +0100

    cicle for of list of lives

--- js/pacman.js
+++ js/pacman.js
@@ -1,11 +1,28 @@
-
+let vius = document.getElementById("vius");
 let arrayNames = ["Ingrid", "Uri", "Guillermo", "Ales", "Albert", "Adrian", "Dani", "Yeray", "Jess", "Alex", "Scarlet","Sergi"];
-let result;
+
 function randomArrayNames() {
     let random = Math.floor(Math.random()*arrayNames.length);
     console.log(random);
     let itemExtract = arrayNames[random];
     arrayNames.splice(random, 1);
     document.getElementById("names").innerHTML= itemExtract;
-    document.getElementById("list").innerHTML+=`<li>${itemExtract}</li>`;
+    document.getElementById("morts").innerHTML+=`<li>${itemExtract}</li>`;
 }
+
+// cicle for
+for (let i = 0; i < arrayNames.length; i++){
+    vius.innerHTML += `<li>${arrayNames[i]}</li>`;
+    // vius.innerHTML = vius.innerHTML + `<li>${arrayNames[i]}</li>`;
+}
+
+
+/* // explicacio de for
+let posicio = 0;
+vius.innerHTML += `<li>${arrayNames[posicio]}</li>`;
+
+posicio = posicio + 1; // 1
+vius.innerHTML += `<li>${arrayNames[posicio]}</li>`;
+
+posicio = posicio + 1; // 2
+vius.innerHTML += `<li>${arrayNames[posicio]}</li>`; */
