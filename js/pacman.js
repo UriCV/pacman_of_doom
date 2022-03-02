4af829dac2c2bef91486028a934446186b6ef0c5
Ales <aleswebgit@gmail.com>
2022-03-02 15:32:25 +0100

    limit remove and function print lives
    Co-authored-by: MADROCHA <MADROCHA@users.noreply.github.com>

--- js/pacman.js
+++ js/pacman.js
@@ -2,20 +2,27 @@ let vius = document.getElementById("vius");
 let arrayNames = ["Ingrid", "Uri", "Guillermo", "Ales", "Albert", "Adrian", "Dani", "Yeray", "Jess", "Alex", "Scarlet","Sergi"];
 
 function randomArrayNames() {
-    let random = Math.floor(Math.random()*arrayNames.length);
-    console.log(random);
-    let itemExtract = arrayNames[random];
-    arrayNames.splice(random, 1);
-    document.getElementById("names").innerHTML= itemExtract;
-    document.getElementById("morts").innerHTML+=`<li>${itemExtract}</li>`;
+    if (arrayNames.length !== 0) {
+        let random = Math.floor(Math.random()*arrayNames.length);
+        console.log(random);
+        let itemExtract = arrayNames[random];
+        arrayNames.splice(random, 1);
+        printLives();
+        document.getElementById("names").innerHTML= itemExtract;
+        document.getElementById("morts").innerHTML+=`<li>${itemExtract}</li>`;
+    }
 }
 
 // cicle for
-for (let i = 0; i < arrayNames.length; i++){
-    vius.innerHTML += `<li>${arrayNames[i]}</li>`;
-    // vius.innerHTML = vius.innerHTML + `<li>${arrayNames[i]}</li>`;
+function printLives() {
+    vius.innerHTML = "";
+    for (let i = 0; i < arrayNames.length; i++){
+        vius.innerHTML += `<li>${arrayNames[i]}</li>`;
+        // vius.innerHTML = vius.innerHTML + `<li>${arrayNames[i]}</li>`;
+    }
 }
 
+printLives();
 
 /* // explicacio de for
 let posicio = 0;
