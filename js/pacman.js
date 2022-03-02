7c2aa94c4bb4f768f6b0dd63cdc935591b1f6c5c
Ales <aleswebgit@gmail.com>
2022-03-02 12:23:52 +0100

    fix randomArrayNames

--- js/pacman.js
+++ js/pacman.js
@@ -1,15 +1,11 @@
 
 let arrayNames = ["Ingrid", "Uri", "Guillermo", "Ales", "Albert", "Adrian", "Dani", "Yeray", "Jess", "Alex", "Scarlet","Sergi"];
 let result;
-function randomArrayNames(){
-    console.log(arrayNames);
-    let result = arrayNames[Math.floor(Math.random()*arrayNames.length)];
-    console.log("Abans d'splice: " + result);
-    let splicedName = arrayNames.splice(result, 1);
-    console.log("Després d'splice: " + result);
-    console.log('aa',delete result);
-    console.log(splicedName);
-    document.getElementById("names").innerHTML=result;
-    document.getElementById("list").innerHTML+=`<li>${result}</li>`;
+function randomArrayNames() {
+    let random = Math.floor(Math.random()*arrayNames.length);
+    console.log(random);
+    let itemExtract = arrayNames[random];
+    arrayNames.splice(random, 1);
+    document.getElementById("names").innerHTML= itemExtract;
+    document.getElementById("list").innerHTML+=`<li>${itemExtract}</li>`;
 }
-
